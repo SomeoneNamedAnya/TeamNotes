a8e74ccacdc398496320e239ab7ffc52bee0226e
Anna <annamyagkova14@gmail.com>
2024-07-12 15:58:06 +0300

    Getting and sending data from backend

--- frontend/src/Entrance/Entrance.jsx
+++ frontend/src/Entrance/Entrance.jsx
@@ -2,6 +2,21 @@ import style from "./entrance.module.css"
 
 const Entrance = () => {
 
+    const formHandler = async () => {
+        const data = await (await fetch('http://localhost:3005/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({
+                email: 'some@email.ru'
+            })
+        })).json();
+
+        console.log(data);
+
+    }
+
     return (
         <div>
             <div className={style.form}>
@@ -10,7 +25,7 @@ const Entrance = () => {
                     <input type='text' autocomplete="on" placeholder="Имя"></input>
                     <input type='email' autocomplete="on" placeholder="Почта"></input>
                     <input type="password" autocomplete="on" placeholder="Пароль"></input>
-                    <button className={style.subButton}>Регистрация</button>
+                    <button className={style.subButton} onClick={formHandler} type="button">Регистрация</button>
                 </form>
             </div>
         </div> 
