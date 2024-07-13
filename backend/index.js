9ccb13d6ef810e6a6a3290bb63730a523172ca3d
matveevaolga <matveevaolga2882@gmail.com>
2024-07-13 20:12:47 +0300

    Processing adding users's data to database.
    Checking if the currently registering user entered the email that is already present in database.

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,16 @@ require('dotenv').config();
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3006;
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  insecureAuth : true
+});
+db.connect();
 
 app.use(express.json());
 app.use(cors());
@@ -21,8 +31,25 @@ app.post('/signUp', (req, res) => {
     console.log(name);
     console.log(email);
     console.log(password);
-    //Обработки// 
-    res.status(201).json({email: `Your email is ${email}`});  //То, что мы отправляем на клиент 
+    //Обработки//
+    db.query("SELECT * FROM users WHERE email = ?",
+      [email], (err, result) => {
+      console.log(err);
+      console.log(result);
+      if (!err && result && !result.length) {
+        console.log("успешная регистрация");
+        db.query("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, password]);
+        // res.status(201).json({email: `Вы успешно зарегистрировались под почтой ${email}`});
+      }
+      else if (result && result.length) {
+        console.log("уже зарегистрирован")
+        // res.status(201).json({email: `Пользователь с почтой ${email} уже зарегистрирован`});  //То, что мы отправляем на клиент
+      }
+      else {
+        // res.status(400).json({email: `При регистрации произошла ошибка $`});
+        console.log("произошла ошибка");
+      }
+    });
 })
 
 app.listen(port, () => {
