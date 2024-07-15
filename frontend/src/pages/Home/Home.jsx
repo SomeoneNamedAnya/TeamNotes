0d9a0e26caf204c2040810dfd24703b0b84090b8
Anna <annamyagkova14@gmail.com>
2024-07-15 14:02:33 +0300

    Home page. First changes

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,14 +1,80 @@
 import style from "./home.module.css"
+import React, { useState } from 'react';
+
+import type { MenuProps } from 'antd';
+import {
+    AppstoreOutlined,
+    ContainerOutlined,
+    DesktopOutlined,
+    MailOutlined,
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    PieChartOutlined,
+  } from '@ant-design/icons';
+import { Button, Flex, Layout, Menu } from 'antd';
+import SizeContext from "antd/es/config-provider/SizeContext";
+const { Header, Footer, Sider, Content } = Layout;
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+const items: MenuItem[] = [
+    { key: '1',  label: 'Группы' },
+    { key: '2',  label: 'Приглашения' },
+  ];
+
+const headerStyle = {
+    textAlign: 'center',
+    color: '#FFE7E7',
+    height: "20vh",
+    width: "100vw",
+    paddingInline: 48,
+    lineHeight: '64px',
+    fontSize: "50px",
+    backgroundColor: '#944E63',
+};
+
+  const layoutStyle = {
+    borderRadius: 8,
+    height: "100vh",
+    overflow: 'hidden',
+    width: "100vw",
+    maxWidth: '100vw',
+    backgroundColor: '#B47B84',
+  };
 
 const Home = () => {
+    const [collapsed, setCollapsed] = useState(false);
+
+    const toggleCollapsed = () => {
+        setCollapsed(!collapsed);
+    };
     
     return (
-        <div>
-            <div className={style.form}>
-            <h1>HomePage</h1>
+        <Flex>
+            <Layout style={layoutStyle}>
                 
-            </div>
-        </div> 
+                
+                <Sider
+                breakpoint="lg"
+                collapsedWidth="0"
+                onBreakpoint={(broken) => {
+                console.log(broken);
+                }}
+                onCollapse={(collapsed, type) => {
+                console.log(collapsed, type);
+                }}
+                >
+                <div className="demo-logo-vertical" />
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+                </Sider>
+                <Layout>
+                    <Header style={headerStyle}>
+                        Мои группы
+                    </Header>
+                </Layout>
+                
+            </Layout>
+        </Flex>
     )
 };
 export default Home;
--- frontend/src/pages/Registration/Registration.jsx
+++ frontend/src/pages/Registration/Registration.jsx
@@ -6,7 +6,7 @@ const Registration = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const formHandler = async (elem) => {
-        const data = await (await fetch('http://localhost:3005/login', {
+        const data = await (await fetch('http://localhost:3006/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
@@ -20,23 +20,21 @@ const Registration = () => {
     })).json();
     document.getElementById("result").innerHTML = data.email;
 }
-};
+return (
+    <div>
+        <div className={style.form}>
+        <p>Создать аккаунт</p>
+            <form  className={style.reg}>
+                <input type='text' autoComplete="on" placeholder="Имя" onChange={e => setName(e.target.value)}></input>
+                <input type='email' autoComplete="on" placeholder="Почта" onChange={e => setEmail(e.target.value)}></input>
+                <input type="password" autoComplete="on" placeholder="Пароль" onChange={e => setPassword(e.target.value)}></input>
+                <button className={style.subButton} onClick={formHandler} type="button">Регистрация</button>
+                <p id="result">Результат регистрации</p>
+            </form>
+        </div>
+    </div> 
+)
+}
 
-const Entrance = () => {
-    const [n, setName] = useState("");
-    return (
-        <div>
-            <div className={style.form}>
-            <p>Создать аккаунт</p>
-                <form  className={style.reg}>
-                    <input type='text' autocomplete="on" placeholder="Имя" onChange={e => setName(e.target.value)}></input>
-                    <input type='email' autocomplete="on" placeholder="Почта" onChange={e => setEmail(e.target.value)}></input>
-                    <input type="password" autocomplete="on" placeholder="Пароль" onChange={e => setPassword(e.target.value)}></input>
-                    <button className={style.subButton} onClick={formHandler} type="button">Регистрация</button>
-                    <p id="result">Результат регистрации</p>
-                </form>
-            </div>
-        </div> 
-    )
-};
+
 export default Registration;
