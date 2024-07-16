f932dfd008e7cc0218d6ed86394eede558480e85
Anna <annamyagkova14@gmail.com>
2024-07-16 12:09:42 +0300

    Update home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,15 +1,14 @@
-import style from "./home.module.css"
+import {Link, useNavigate} from "react-router-dom";
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Entrance from "../Entrance/Entrance.jsx"
 import type { MenuProps } from 'antd';
 import {
     TeamOutlined,
     MailOutlined,
-    HomeOutlined
+    HomeOutlined,
+    QuestionCircleOutlined
   } from '@ant-design/icons';
 import {Breadcrumb, Button, Flex, Table, Layout, Menu, theme,Typography } from 'antd';
-import SizeContext from "antd/es/config-provider/SizeContext";
+const { Header, Footer, Sider, Content } = Layout;
 
 const dataSource = [
     {
@@ -29,65 +28,54 @@ const dataSource = [
         time: "16.07.2024",
         add: "?",
     },
-  ];
+];
   
-  const columns = [
-    {
-      title: 'Название группы',
-      dataIndex: 'name',
-      key: 'name',
+const columns = [
+{
+    title: 'Название группы',
+    dataIndex: 'name',
+    key: 'name',
+},
+{
+    title: 'Создатель группы',
+    dataIndex: 'author',
+    key: 'author',
     },
-    {
-        title: 'Создатель группы',
-        dataIndex: 'author',
-        key: 'author',
-      },
-    {
-        title: 'Описание',
-        dataIndex: 'description',
-        key: 'description',
-    },
-    {
-      title: 'Время создания',
-      dataIndex: 'time',
-      key: 'time',
-    },
-    {
-      title: 'Дополнительно',
-      dataIndex: 'add',
-      key: 'add',
-    },
-  ];
-
-const { Header, Footer, Sider, Content } = Layout;
-
-type MenuItem = Required<MenuProps>['items'][number];
-
-const items: MenuItem[] = [
-    { key: '1',  icon:<TeamOutlined />, label: 'Группы' },
-    { key: '2',  icon:<MailOutlined />, label: 'Приглашения' },
-    { key: '3',  icon:<HomeOutlined />, label: 'Выход' },
-  ];
-
-//const headerStyle = 
-
-  const layoutStyle = {
-    borderRadius: 8,
-    height: "100vh",
-    overflow: 'hidden',
-    width: "100vw",
-    maxWidth: '100vw',
-    backgroundColor: '#B47B84',
-  };
+{
+    title: 'Описание',
+    dataIndex: 'description',
+    key: 'description',
+},
+{
+    title: 'Время создания',
+    dataIndex: 'time',
+    key: 'time',
+},
+{
+    title: 'Дополнительно',
+    dataIndex: 'add',
+    key: 'add',
+},
+];
 
 const Home = () => {
+    const navigate = useNavigate();
+    type MenuItem = Required<MenuProps>['items'][number];
+
+    const items: MenuItem[] = [
+        { key: '1',  icon:<TeamOutlined />, label: 'Группы', onClick:() => {navigate("/home")} },
+        { key: '2',  icon:<MailOutlined />, label: 'Приглашения', onClick:() => {navigate("/invitation")}  },
+        { key: '3',  icon:<QuestionCircleOutlined />, label: 'О приложении',  onClick:() => {navigate("/about")}},
+        { key: '4',  icon:<HomeOutlined />, label: 'Выход',  onClick:() => {navigate("/entrance")}},
+    ];
+
     const [collapsed, setCollapsed] = useState(false);
-    let innerTextName = "ДРУГОЕ";
-    let innerTextEmail = "user@email.com";
-    //console.log("aaaaaa");
+
+    let innerTextName;
+    let innerTextEmail;
+
     useEffect(() => {
-        console.log("12345");
-        // Обновляем заголовок документа, используя API браузера
+        
         if (collapsed) {
             innerTextName = "";
             innerTextEmail = ""//"ДРУГОЕ";
@@ -98,11 +86,11 @@ const Home = () => {
         
         document.getElementById("userName").innerHTML = innerTextName;
         document.getElementById("userEmail").innerHTML = innerTextEmail;
+
       });
-    
     const {
         token: { colorBgContainer, borderRadiusLG },
-      } = theme.useToken();
+    } = theme.useToken();    
     
     return (
       
@@ -110,25 +98,24 @@ const Home = () => {
             minHeight: '100vh',
           }}>
             
-            <Sider /*style={siderStyle}*/ collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-                <div className="demo-logo-vertical" />
+            <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+                
                 <div style={{margin:"10px 0 10px 10px"}}>
-                <div id="userName" style={{color:"white", fontSize: "25px", display:Flex}}>
-                {innerTextName}
+                    <div id="userName" style={{color:"white", fontSize: "25px", display:Flex}}>
+                        {innerTextName}
+                    </div>
+                    <div id="userEmail" style={{color:"gray", fontSize: "15px", display:Flex}}>
+                        {innerTextEmail}
+                    </div>
                 </div>
-                <div id="userEmail" style={{color:"gray", fontSize: "15px", display:Flex}}>
-               
-                {innerTextEmail}
-                </div>
-                </div>
-                    
-            
+        
                 <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
                 
             </Sider>
+
             <Layout>
-                <Header style={
-                {
+
+                <Header style={{
                     display:Flex,
                     textAlign: "center",
                     justify_content: "center",
@@ -141,15 +128,16 @@ const Home = () => {
                 }}>
                     Мои группы
                 </Header>
+
                 <Content style={{
-                        margin: '0 16px',
-                        height: "100%"
+                    margin: '0 16px',
+                    height: "100%",
                 }}>
                     <Table dataSource={dataSource} columns={columns} />;
-                   
                 </Content>
                 
             </Layout>
+
         </Layout>
        
     )
